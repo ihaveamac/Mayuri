2f0cc39680779a39a404d2b4488049f3be3db5ff
ihaveamac <ian@ianburgwin.net>
2017-04-05 00:41:39 -0700

    remove test commands

--- cogs/extras.py
+++ cogs/extras.py
@@ -21,17 +21,6 @@ class Extras:
             "**<https://github.com/ihaveamac/Mayuri>**!"
         )
 
-    @check.fail()
-    @commands.command()
-    async def failtest(self):
-        """Always fail with no permission for testing purposes"""
-        await self.bot.say("This message should not happen")
-
-    @commands.command()
-    async def errortest(self):
-        """Always raise an exception for testing purposes"""
-        raise Exception("intended exception")
-
     @commands.command(pass_context=True)
     async def welcometest(self, ctx):
         """Test welcome text."""
@@ -42,28 +31,6 @@ class Extras:
             ))
         else:
             await self.bot.say("WELCOME_MESSAGE is empty!")
-
-    @commands.command(pass_context=True)
-    async def a(self, ctx):
-        await self.bot.say(self.bot.format_message(
-            "username={username}\n"
-            "username_mention={username_mention}\n"
-            "server_name={server_name}\n"
-            "welcome_channel={welcome_channel}\n"
-            "welcome_channel_mention={welcome_channel_mention}\n"
-            "command={command}\n"
-            "command_prefixed={command_prefixed}\n"
-            "prefix={prefix}\n",
-            ctx.message.author,
-            ctx=ctx
-        ))
-
-    @commands.command(pass_context=True)
-    async def b(self, ctx, user, reason):
-        await log.log('mod', self.bot.channels['mod-logs'], ctx=ctx,
-                      target=ctx.message.mentions[0], action='Action',
-                      action_message='Action message', user=ctx.message.author,
-                      reason=reason, emoji='FACE WITH TEARS OF JOY')
 
 
 def setup(bot):
